7adb8898072ed74e1755e3848e397ea583657901
Sushil Thapa <sushilt553@gmail.com>
2019-09-26 20:38:05 +0000

    Automatically backed up by Learn

--- arrays.js
+++ arrays.js
@@ -38,3 +38,8 @@ function destructivelyRemoveElementFromEndOfArray(chocolateBars){
   chocolateBars.pop();
   return chocolateBars;
 }
+
+function removeElementFromEndOfArray(chocolateBars){
+  chocolateBars.slice(0,3);
+  return chocolateBars;
+}
